bd66c915cce08429c5a74e4e3e1b48c80b319740
NEYAZ N <neyaznafiz@outlok.com>
2023-06-18 12:58:18 +0600

    rectangle

---
+++ target/debug/deps/rectangle_calculate.d
@@ -0,0 +1,5 @@
+D:\Documents\Programming\Rust\rust-learning\rectangle-calculate\target\debug\deps\rectangle_calculate.exe: src\main.rs
+
+D:\Documents\Programming\Rust\rust-learning\rectangle-calculate\target\debug\deps\rectangle_calculate.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/rectangle-calculate.d
@@ -0,0 +1,1 @@
+D:\Documents\Programming\Rust\rust-learning\rectangle-calculate\target\debug\rectangle-calculate.exe: D:\Documents\Programming\Rust\rust-learning\rectangle-calculate\src\main.rs
